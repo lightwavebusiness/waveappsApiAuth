f3c426696f6c9fe82c7e7b0712f6fde1401ebcd6
Leon Chaffey <leon@l-w-b.co>
2018-09-05 07:51:00 +0100

    .env setup

--- index.js
+++ index.js
@@ -5,56 +5,58 @@ const crypto = require("crypto");
 const nonce = require('nonce');
 const cookie = require('cookie');
 
+require('dotenv').config()
+
 const app = express();
+
 const baseUrl = "https://api.waveapps.com/";
+const scopes = "basic user.read user.write business.read business.write account.read account.write invoice.read invoice.write";
+
 
 var cryptoToken = crypto.randomBytes(64).toString('hex');
 
-var config = {
-    "wave" : {
-        "client_id" : "EgzhF7-HiXtmKg7HwpIjTT-m1huTMC0YtV0KudnA",
-        "client_secret" : "It9OGuJ2LON:_JeevNOv8b.4-h@hbIG3srrKoIFBiC!NZr?GobrTi339WMdv6sok?QngwoRrw5Olxx7hWaRQwb.Ag37sBtMsbs-fzbTYUK0arkj-Z4_6Cms1-o@0n_C2"
-    }
-}
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', (req, res) => {
+    let accessToken = cookie.parse(req.headers.cookie || '').accessToken
+    if(accessToken===undefined) {
+       res.redirect("/request",)
+    }
+    res.send('Hello World!')}
+)
 
 app.get('/request', (req, res) => {
     var redirectUrl = `${req.protocol}://${req.get('host')}/token`;
-console.log(redirectUrl);
 
     res.redirect(url.format({
         pathname:`${baseUrl}oauth2/authorize/`,
         query: {
-           "client_id": config.wave.client_id,
+           "client_id": process.env.wave_client_id,
            "redirect_uri": redirectUrl,
            "response_type":"code",
-           "scope": "basic",
+           "scope": scopes,
            "state": cryptoToken
          }
       }));
 })
-
-
-//  user.read user.write	business.read business.write account.read account.write invoice.read invoice.write
 
 app.get("/token", (req, res) => {
     console.log(req.query.code);
 
     //var redirectUrl = `${req.protocol}://${req.get('host')}/`;
 
-    var redirectUrl = `${req.protocol}://${req.get('host')}/oauth2/token`;
+    var redirectUrl = `${req.protocol}://${req.get('host')}/token`;
     console.log(redirectUrl);
 
-    var reqUrl = `${baseUrl}oauth2/token`;
+    var reqUrl = `${baseUrl}oauth2/token/`;
     console.log(reqUrl);
 
     var options = {
         uri: reqUrl,
         method: 'POST',
         form: {
-            "client_id": config.wave.client_id,
-            "client_secret": config.wave.client_secret,
+            "client_id": process.env.wave_client_id,
+            "client_secret": process.env.wave_client_secret,
             "code": req.query.code,
             "redirect_uri" : redirectUrl,
             'grant_type': 'authorization_code'
